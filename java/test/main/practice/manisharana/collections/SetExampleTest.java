a1b5cf96ac664a9ec3ba8414a7fbb9f29b76a1f3
ManishaRana1195 <manisha1195@gmail.com>
2019-01-03 16:02:21 -0600

    set headset, tailset, iterator

--- java/test/main/practice/manisharana/collections/SetExampleTest.java
+++ java/test/main/practice/manisharana/collections/SetExampleTest.java
@@ -1,9 +1,11 @@
 package main.practice.manisharana.collections;
 
 import org.junit.Test;
+import org.testng.annotations.BeforeTest;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import static org.testng.Assert.*;
@@ -27,6 +29,7 @@ public class SetExampleTest {
         set.add("mango");
         set.add("wobbuffet");
         set.add("crill");
+        set.add("angle");
         return set;
     }
 
@@ -35,6 +38,7 @@ public class SetExampleTest {
         TreeSet<String> set = getSetData();
         Iterator<String> iterator = set.iterator();
 
+        int size = set.size();
         while(iterator.hasNext()){
             String next = iterator.next();
             if(next.equals("mango")){
@@ -43,7 +47,47 @@ public class SetExampleTest {
             System.out.println(next);
         }
 
-        assertEquals(set.size(), 3);
+        assertEquals(set.size(), size-1);
     }
 
+    @Test
+    public void should_return_subset_elements(){
+        TreeSet<String> set = getSetData();
+        TreeSet<String> expected = new TreeSet<>();
+        expected.add("python");
+        expected.add("mango");
+
+        SortedSet<String> actual = set.subSet("mango", "wobbuffet");
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void when_using_headset_should_return_head_elements(){
+        TreeSet<String> set = getSetData();
+        TreeSet<String> expected = new TreeSet<>();
+        expected.add("angle");
+        expected.add("crill");
+        expected.add("mango");
+        SortedSet<String> actual = set.headSet("python");
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void when_using_tailset_should_return_tail_elements(){
+        TreeSet<String> setData = getSetData();
+        TreeSet<String> expected = new TreeSet<>();
+        expected.add("wobbuffet");
+        expected.add("python");
+
+        SortedSet<String> actual = setData.tailSet("python");
+        assertEquals(actual,expected);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void when_adding_null_should_raise_null_pointer(){
+        TreeSet<String> setData = getSetData();
+        setData.add(null);
+    }
 }
