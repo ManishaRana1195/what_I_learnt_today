437ec5217c19486957dab423dc164f04a095eaba
ManishaRana1195 <manisha1195@gmail.com>
2019-01-08 23:31:57 -0600

    my first ML program

---
+++ python/machine_learning/naive_bayes_gaussian.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+X = np.array([[-1, -1], [-2, -1], [1, 1], [-3, -2], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+
+from sklearn.naive_bayes import GaussianNB
+
+# creating the classifier
+clf = GaussianNB()
+
+clf.fit(X, Y)
+print(clf.predict([[-0.8, -1]]))
