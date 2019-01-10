99498acf4018038d6b45bb19b6503beeefd7c08a
ManishaRana1195 <manisha1195@gmail.com>
2019-01-10 13:20:49 -0600

    Refactor Object parser

--- java/src/main/practice/manisharana/jsonParser/ObjectParser.java
+++ java/src/main/practice/manisharana/jsonParser/ObjectParser.java
@@ -1,61 +1,68 @@
 package main.practice.manisharana.jsonParser;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class ObjectParser implements Parser {
 
     @Override
-    public JsonObject parse(String input){
-        int i = 0;
+    public JsonObject parse(String input) {
         /*To see if number of brackets are balanced*/
         Stack<Character> bracketStack = new Stack<>();
-        JsonObject jsonObject = new JsonObject("",input);
-        StringBuilder result = new StringBuilder();
+        JsonObject resultJsonObject = new JsonObject("", input);
 
-        while(input.length() > 0){
+        StringBuilder result = parseAndGetJsonObject(input, bracketStack);
 
-            String character = String.valueOf(input.charAt(i));
-            if(character.equals("{")){
-                bracketStack.push(input.charAt(i));
-                result.append(character);
-                input = input.substring(i+1);
-            }
-
-            switch (String.valueOf(input.charAt(i))) {
-                case " ":
-                    jsonObject = new SpaceParser().parse(input);
-                    break;
-                case ":":
-                    jsonObject = new ColonParser().parse(input);
-                    break;
-                case ",":
-                    jsonObject = new CommaParser().parse(input);
-                    break;
-                default:
-                    jsonObject = new StringParser().parse(input);
-                    // could be number parser
-                    // could be boolean parser
-                    // could be array parser
-                    break;
-            }
-            /*updating the input string*/
-            input = jsonObject.getUnparsedString();
-
-            /*append the parsed string together*/
-            result.append(jsonObject.getObject());
-
-            String nextChar = String.valueOf(input.charAt(i));
-            if(nextChar.equals("}")){
-                bracketStack.pop();
-                result.append(nextChar);
-                input = input.substring(i+1);
-            }
-        }
-
-        if(!bracketStack.empty()){
+        if (!bracketStack.empty()) {
             // raise exception parsing error
         }
-        jsonObject.setObject(result.toString());
-        return jsonObject;
+        resultJsonObject.setObject(result.toString());
+        return resultJsonObject;
+    }
+
+    private StringBuilder parseAndGetJsonObject(String input, Stack<Character> bracketStack) {
+        int i = 0;
+        StringBuilder result = new StringBuilder();
+        JsonObject tempJsonObject;
+        ArrayList<Parser> parserList = getParserList();
+
+
+        while (input.length() > 0) {
+            String character = String.valueOf(input.charAt(i));
+            if (character.equals("{")) {
+                bracketStack.push(input.charAt(i));
+                result.append(character);  // tempJsonObject.setObject(character);
+                input = input.substring(i + 1);
+            }
+
+            for (Parser parser : parserList) {
+                tempJsonObject = parser.parse(input);
+                if(tempJsonObject != null){
+                    input = tempJsonObject.getUnparsedString();
+                    result.append(tempJsonObject.getObject());
+                }
+            }
+
+            String nextChar = String.valueOf(input.charAt(i));
+            if (nextChar.equals("}")) {
+                bracketStack.pop();
+                result.append(nextChar);
+                input = input.substring(i + 1);
+            }
+        }
+
+        return result;
+    }
+
+    private ArrayList<Parser> getParserList() {
+        ArrayList<Parser> parsers = new ArrayList<>();
+        parsers.add(new SpaceParser());
+        parsers.add(new ColonParser());
+        parsers.add(new CommaParser());
+        parsers.add(new StringParser());
+        parsers.add(new SpaceParser());
+        parsers.add(new NumberParser());
+        parsers.add(new BooleanParser());
+        return parsers;
     }
 }
