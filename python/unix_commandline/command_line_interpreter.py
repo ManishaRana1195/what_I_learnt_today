3e314b190b5291b26ee6c078c4f29d321d900de4
ManishaRana1195 <manisha1195@gmail.com>
2019-01-11 23:52:35 -0600

    getting env variable for command path

--- python/unix_commandline/command_line_interpreter.py
+++ python/unix_commandline/command_line_interpreter.py
@@ -1,4 +1,5 @@
 import os
+from os import environ
 
 
 def change_directory_command(input_command):
@@ -27,6 +28,7 @@ def processes_command(running_processes):
 cd = "cd"
 exiting = "exit"
 processes = "processes"
+bg = "bg"
 built_in_commands = {cd: change_directory_command, exiting: exit_command, processes: processes_command}
 
 
@@ -46,11 +48,24 @@ def is_built_in(input_command):
     return False
 
 
+def get_path_to_command(input_command):
+    command_paths = environ['PATH']
+    print(command_paths)
+
+
 def main():
     user_input = input("Enter command>>>").strip()
     running_processes = []
+    is_background_process = False
+    if user_input.startswith(bg):
+        is_background_process = True;
+
     if is_built_in(user_input):
         handle_built_in_commands(user_input, running_processes)
+    else:
+        get_path_to_command(user_input)
+
+
 
 
 if __name__ == "__main__":
