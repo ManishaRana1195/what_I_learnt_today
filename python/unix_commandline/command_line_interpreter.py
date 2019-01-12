172262e0de2dff8302f948f66a6e9cbc4128f288
ManishaRana1195 <manisha1195@gmail.com>
2019-01-11 22:47:00 -0600

    added running_processes array to maintain the backgraound processes

--- python/unix_commandline/command_line_interpreter.py
+++ python/unix_commandline/command_line_interpreter.py
@@ -5,8 +5,7 @@ def change_directory_command(input_command):
     print("current path: {} ".format(os.getcwd()))
 
     cd_index = input_command.find(cd)
-    path = input_command[cd_index+len(cd):]
-    #print(path)
+    path = input_command[cd_index + len(cd):]
     try:
         os.chdir(path.strip())
     except OSError:
@@ -15,11 +14,13 @@ def change_directory_command(input_command):
     print("current path: {} ".format(os.getcwd()))
 
 
-def exit_command(input_command):
+def exit_command(running_processes):
     print("exit !!")
 
 
-def processes_command(input_command):
+def processes_command(running_processes):
+    for process in running_processes:
+        print("background process with pid {} running".format(process))
     print("processes")
 
 
@@ -29,10 +30,13 @@ processes = "processes"
 built_in_commands = {cd: change_directory_command, exiting: exit_command, processes: processes_command}
 
 
-def handle_built_in_commands(input_command):
-    for command in built_in_commands.keys():
-        if input_command.startswith(command):
-            built_in_commands[command](input_command)
+def handle_built_in_commands(input_command, running_processes):
+    if input_command.startswith(cd):
+        built_in_commands[cd](input_command)
+    elif input_command.startswith(exiting):
+        built_in_commands[exiting](running_processes)
+    elif input_command.startswith(processes):
+        built_in_commands[exiting](running_processes)
 
 
 def is_built_in(input_command):
@@ -44,8 +48,9 @@ def is_built_in(input_command):
 
 def main():
     user_input = input("Enter command>>>").strip()
+    running_processes = []
     if is_built_in(user_input):
-        handle_built_in_commands(user_input)
+        handle_built_in_commands(user_input, running_processes)
 
 
 if __name__ == "__main__":
