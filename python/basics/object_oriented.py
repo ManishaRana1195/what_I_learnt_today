5ee611d2ef0ccc147992b8689f31290f2dee10f4
ManishaRana1195 <manisha1195@gmail.com>
2019-01-13 23:48:41 -0600

    oop basics

--- python/basics/object_oriented.py
+++ python/basics/object_oriented.py
@@ -8,9 +8,27 @@ class Kettle(object):
     def __str__(self):
         return " make: {}, price: {}".format(self.make, self.price)
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 11.10)
 print(kenwood)
 
 hamilton = Kettle("hamilton", 12.10)
 print(hamilton)
+
+# Woww!!
+print("Models: {0.make}= {0.price}, {1.make}= {1.price}".format(hamilton, kenwood))
+
+# Amazing
+Kettle.switch_on(hamilton)
+print(hamilton.on)
+
+# Adding instance data attributes to instance as we like
+
+kenwood.power = 2.5
+print(kenwood.power)
+print(hamilton.power)   # error no attribute on Kettle
+
+
