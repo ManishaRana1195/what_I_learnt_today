6a1a53bd7a7dd1b0e6056c1e0fade44d9600302c
ManishaRana1195 <manisha1195@gmail.com>
2019-01-17 23:56:32 -0600

    http server - initial commit

---
+++ python/http_server/http_server.py
@@ -0,0 +1,11 @@
+import socket
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind()
+    sock.listen()
+
+if __name__ == "__main__":
+    main()
