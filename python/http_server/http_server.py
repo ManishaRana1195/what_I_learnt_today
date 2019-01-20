29913d9cd09940f6fff645ebb7203d5f11c1e779
ManishaRana1195 <manisha1195@gmail.com>
2019-01-19 20:09:04 -0600

    Added connection class to read input

--- python/http_server/http_server.py
+++ python/http_server/http_server.py
@@ -1,11 +1,30 @@
 import socket
 
 
+class Connection(object):
+
+    def __init__(self, conn_sock):
+        self.conn_sock = conn_sock
+        self.buffer = ""
+
+    def read_line(self):
+        while "\r\n" not in self.buffer:
+            self.buffer += repr(self.conn_sock.recv(7))
+            print(self.buffer)
+
+
 def main():
+    # AF_INET for internet socket
+    # STREAM, not raw
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind()
-    sock.listen()
+    sock.bind(("127.0.0.1", 4000))
+    sock.listen(0)
+    # accept blocks and waits for client request
+    client_sock, address_info = sock.accept()
+   # print(client_sock.recv(4096))
+    Connection(client_sock).read_line()
+
 
 if __name__ == "__main__":
     main()
