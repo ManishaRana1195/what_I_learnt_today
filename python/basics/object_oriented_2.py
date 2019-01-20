1fa4674da84d1530eae60e059d522b617ab9d559
ManishaRana1195 <manisha1195@gmail.com>
2019-01-19 23:26:03 -0600

    static methods in python

--- python/basics/object_oriented_2.py
+++ python/basics/object_oriented_2.py
@@ -1,4 +1,20 @@
+import datetime
+import pytz
+
+
 class Account(object):
+    withdraw_action = "withdrawn"
+    deposit_action = "deposited"
+
+    @staticmethod
+    def _current_time():
+        """
+            creating static method in python
+            starting private methods with single _ means method is not
+            intended to be accessed outside the class.
+        """
+        utc_time = datetime.datetime.now()
+        return pytz.utc.localize(utc_time)
 
     # new method is the constructor, used to
     # instantiate the class
@@ -8,18 +24,27 @@ class Account(object):
     # init method is to customize the object with values
     def __init__(self, name, balance):
         self.name = name
+        self.transaction_list = []
         self.balance = balance
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.transaction_list.append((Account._current_time(), amount, self.deposit_action))
 
     def withdraw(self, amount):
         if amount > 0 and (self.balance - amount) >= 0:
             self.balance -= amount
+            self.transaction_list.append(
+                (pytz.utc.localize(datetime.datetime.now()), amount * -1, self.withdraw_action))
 
     def show_balance(self):
         print(" Account balance: {} ".format(self.balance))
+
+    def show_transactions(self):
+        for date, amount, action in self.transaction_list:
+            print(" {} {} on {} (local time was {} )".format(amount, action, date, date.astimezone()))
+
 
 if __name__ == "__main__":
     mark = Account("Mark", 400)
@@ -29,3 +54,5 @@ if __name__ == "__main__":
     mark.show_balance()
     mark.withdraw(500)
     mark.show_balance()
+
+    mark.show_transactions()
