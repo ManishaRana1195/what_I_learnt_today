b7d875b88c8cf25a4c01d4f96b5d669853b80ec0
ManishaRana1195 <manisha1195@gmail.com>
2019-01-22 23:36:20 -0600

    docstring example in pytthon

--- python/basics/object_oriented_3.py
+++ python/basics/object_oriented_3.py
@@ -6,6 +6,7 @@ class Song(object):
         artist(Artist) : The Artist object
         duration(int): The duration in seconds
     """
+
     def __init__(self, title, artist, duration=0):
         """ Song init method
 
@@ -16,3 +17,41 @@ class Song(object):
         self.title = title
         self.artist = artist
         self.duration = duration
+
+
+# methods to display the docstring
+# help(Song.__init__)
+# print(Song.__doc__)
+# print(Song.__init__.__doc__)
+
+class Album(object):
+    """
+        Class to represent an Album
+        Attributes:
+            name(str): The name of the album
+            year(int): year the album was released
+            artist(Artist): The artist/creator of the album, defaults to
+            "Various Artists"
+            tracks(List[Song]): list of songs of album
+    """
+
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        self.tracks = []
+        if artist is None:
+            self.artist = Artist("Various Artists")
+        else:
+            self.artist = artist
+
+    def add_song(self, song, position=None):
+        """add song to the album
+
+        :param song: song object in album
+        :param position: the position is Optional. If position is not specified, song is appended
+        :return:
+        """
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position, song)
