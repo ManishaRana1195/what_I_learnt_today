972502a405ef48dcdd51dfc34c180622f19e6ad3
ManishaRana1195 <manisha1195@gmail.com>
2019-02-02 23:44:25 -0600

    Text Editor - Putting console input in raw mode

--- ruby/Editor.rb
+++ ruby/Editor.rb
@@ -1,4 +1,28 @@
 #!/usr/bin/env ruby
 
 class Editor
+
+    def initialize
+        lines = File.readlines("sample.txt").map do |line|
+	   line.sub(/\n$/, "")
+	end
+    end
+
+    def execute
+	loop do
+	    handle_input
+        end
+    end
+   
+    def handle_input
+        input = $stdin.getc
+        case input
+            when "\C-q" then exit(0)
+        end
+    end
+
 end
+
+
+
+Editor.new.execute
