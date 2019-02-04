75d0c6886808d5a53d1731a552422d9057566e00
ManishaRana1195 <manisha1195@gmail.com>
2019-02-03 21:59:17 -0600

    Editor - terminal in raw mode using IO console

--- ruby/Editor.rb
+++ ruby/Editor.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/env ruby
 
+require "io/console"
+
 class Editor
 
-    def initialize
-        lines = File.readlines("sample.txt").map do |line|
-	   line.sub(/\n$/, "")
-	end
+  def initialize
+    lines = File.readlines("sample.txt").map do |line|
+      line.sub(/\n$/, "")
     end
+  end
 
-    def execute
-	loop do
-	    handle_input
-        end
+  def execute
+    IO.console.raw do
+      loop do
+        handle_input
+      end
     end
-   
-    def handle_input
-        input = $stdin.getc
-        case input
-            when "\C-q" then exit(0)
-        end
+  end
+
+  def handle_input
+    input = $stdin.getc
+    case input
+    when "\C-q" then
+      exit(0)
     end
+  end
 
 end
-
 
 
 Editor.new.execute
