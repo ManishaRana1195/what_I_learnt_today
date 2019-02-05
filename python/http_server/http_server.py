9814b4bc46344e6cf0053877b632d4ba25809b5d
ManishaRana1195 <manisha1195@gmail.com>
2019-02-05 17:00:25 -0600

    sending some dummy reponse to client

--- python/http_server/http_server.py
+++ python/http_server/http_server.py
@@ -1,5 +1,7 @@
 import socket
 
+encoding = 'utf-8'
+
 
 class Connection(object):
 
@@ -11,7 +13,7 @@ class Connection(object):
         separator = "\r\n"
         while separator not in self.buffer:
             # to utf-8 decoding to remove b''
-            self.buffer += str(self.conn_sock.recv(7), 'utf-8')
+            self.buffer += str(self.conn_sock.recv(7), encoding)
 
         result, self.buffer = self.buffer.split(separator, 2)
         return result
@@ -31,7 +33,10 @@ class Connection(object):
 
 
 def respond(client_conn, status_code, content):
-    print(client_conn, status_code, content)
+    client_conn.send(b'HTTP/1.1 200 OK\r\n', 0)
+    # to show that header section of response has ended
+    client_conn.send(b'\r\n', 0)
+    client_conn.send(bytes(content, encoding), 0)
 
 
 def main():
@@ -45,7 +50,7 @@ def main():
     client_sock, address_info = sock.accept()
     client_conn = Connection(client_sock)
     client_conn.read_request()
-    respond(client_conn, 200, "dummy content")
+    respond(client_sock, 200, "dummy content")
 
 
 if __name__ == "__main__":
